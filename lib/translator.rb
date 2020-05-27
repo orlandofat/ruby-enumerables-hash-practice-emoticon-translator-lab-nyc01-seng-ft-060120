52b84308671ca5c6e5572c37afa3353494548c9e
Orlando Challenger <orlandofat@gmail.com>
2020-05-27 04:24:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,14 +15,19 @@ emo
 end
       
 
-def get_japanese_emoticon(file_path, emoticon)
-  emoticons = load_library(file_path)
-  emo = emoticons.values.find {|faces| faces[0] == emoticon}
-  emo == nil ? "Sorry, that emoticon was not found" : emoji[1]
+def get_japanese_emoticon(file_path, eng_emo)
+  # code goes here
+  library = load_library('./lib/emoticons.yml')
+  library.each do |meaning, idioms|
+      return idioms[:japanese] if idioms[:english] == eng_emo
+  end
+  "Sorry, that emoticon was not found"
 end
-
-def get_english_meaning(file_path, emoticon)
-  emoticons = load_library(file_path)
-  emo = emoticons.find {|name, faces| faces[1] == emoticon }
-  emo == nil ? "Sorry, that emoticon was not found" : emoji[0]
-end
+def get_english_meaning(file_path, jap_emo)
+  # code goes here
+  library = load_library('./lib/emoticons.yml')
+  library.each do |meaning, idioms|
+    return meaning if idioms[:japanese] == jap_emo
+  end
+    "Sorry, that emoticon was not found"
+ end 
